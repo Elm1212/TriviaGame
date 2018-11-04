a9b8cfda75b1808b4808f57f9d1f1c7c5d543a1b
Elm <emmal.muniz@gmail.com>
2018-11-03 21:31:00 -0500

    working app, still needs some styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,210 +1,220 @@
 
 //event listeners  start game page
 //$(document).ready(function(){
-   // $("remaining-time").hide();
-  //  $("start").on("click", trivia.startGame);
-  //  $(document).on("click", ".option", trivia.guessChecker);
-//})
+   //("remaining-time").hide();
+  // $("start").on("click", game.startGame);
+  //  $(document).on("click", ".option", game.guessChecker);
+ // })
 
 
 //variables
 
-var game = {
 
     //game settings ---- what game is reset to each time.
     
-    correct: 0,
-    incorrect: 0,
-    unanswered: 0,
-    currentSet: 0,
-    timer: 0,
-    timerOn: false,
-    timerId : '',
+
 
 
 //make into objects... choices into array
-    questions: {
-    q1: "Which Texas city was the first Blockbuster opened in?",
-    q2: "How many U.S. States border the Gulf of Mexico?",
-    q3: "What is the largest city to connect two continents?",
-    q4: "What is the only Sea on Earth with no coastline?",
-    q5: "What is the longest river in the world?",
-    q6: "What Country has the largest land mass?",
-    q7: "",
-    q8: "",
-    q9: "",
-    },
+    $(document).ready(function () {
+    var options = [
+      {
+        question: "Which Texas city was the first Blockbuster opened in?",
+        choice:['Dallas', 'Midland', 'Austin', 'Houston'],
+        answer: 0,
+        photo:"",
+      },
+      {
+        question: "How many U.S.States border the Gulf of Mexico?",
+        choice:['4', '5', '8', '6'],
+        answer: 1,
+        photo:"",
+      },
+      {
+        question: "What is the largest city to connect two continents?",
+        choice:['Pakistan', 'Istanbul', 'Tokyo', 'Brazil'],
+        answer: 1,
+        photo:"",
+      },
+      {
+        question: "What is the only Sea on Earth with no coastline?",
+        choice:['Sargasso Sea', 'Red Sea', 'Arabian Sea', 'Baltic Sea' ],
+        answer: 0,
+        photo:"",
+      },
+      {       
+        question: "What is the longest river in the world?",
+        choice:['Nile', 'Amazon', 'Mississippi', 'Mekong'],
+        answer: 1,
+        photo:"",
+      },
+      {       
+        question: "What Country has the largest land mass?",
+        choice:['Asia', 'Russia', 'United States', 'Canada'],
+        answer: 1,
+        photo:"",
+      },
+      {       
+        question: "The Black Forest is located in which European Country?",
+        choice:['England', 'France', 'Germany', 'Poland'],
+        answer: 2,
+        photo:"",
+      },
+      {       
+        question: "The duck billed platypus is native to what country?",
+        choice:['Antarctica', 'Australia', 'Argentina', 'China'],
+        answer: 1,
+        photo:"",
+      },
+      {       
+        question: "Which Lake is the worlds largest Lake?",
+        choice:['Lake Travis', "Lake Superior", "Lake Malawi", "Lake Toba"],
+        answer: 1,
+        photo:"",
+      }];
 
-    choices: {
-        q1: ['Dallas', 'Midland', 'Austin', 'Houston'],
-        q2: ['4', '5', '8', '6'],
-        q3: ['Pakistan', 'Istanbul', 'Tokyo', 'Brazil'],
-        q4: ['Sargasso Sea', 'Red Sea', 'Arabian Sea', 'Baltic Sea' ],
-        q5: ['Nile', 'Amazon', 'Mississippi', 'Mekong'],
-        q6: ['Asia', 'Russia', 'United States',],
-        q7: [],
-        q8: [],
-        q9: [],
-
-    },
-
-    answers: {
-        q1: "Dallas",
-        q2: "5",
-        q3: "Istanbul",
-        q4: "Sargasso Sea",
-        q5: "Amazon",
-        q6: "",
-        q7: "",
-        q8: "",
-        q9: "",
-    },
-
-
- 
-//Start Game Page display.... push "start" button and then go to game page.
-
-
-/// make a function that loops through questions and display one question with correspoding choices.
-
-// only one answer can be selected.
-
-// Display countdown timer. when timer runs out go to score page
-startGame: function(){
-    // restarting game results
-    game.currentSet = 0;
-    game.correct = 0;
-    game.incorrect = 0;
-    game.unanswered = 0;
-    clearInterval(game.timerId);
-    
-    //game section appear
-    $('#game').show();
-    
-    //  empty last results
-    $('#results').html('');
-    
-    // show timer
-    $('#timer').text(game.timer);
-    
-    // make start button go away
-    $('#start').hide();
-
-    $('#remaining-time').show();
-    
-    // ask question
-    trivia.nextQuestion();
-    
-  },
-  // method to loop through and display questions and options 
-  nextQuestion : function(){
-    
-    // set timer to 20 seconds each question
-    game.timer = 10;
-     $('#timer').removeClass('last-seconds');
-    $('#timer').text(game.timer);
-    
-    // to prevent timer speed up
-    if(!game.timerOn){
-      game.timerId = setInterval(game.timerRunning, 1000);
-    }
-    
-    // gets all the questions then indexes the current questions
-    var questionContent = Object.values(game.questions)[game.currentSet];
-    $('#question').text(questionContent);
-    
-    // an array of all the user options for the current question
-    var questionOptions = Object.values(game.options)[game.currentSet];
-    
-    // creates all the trivia guess options in the html
-    $.each(questionOptions, function(index, key){
-      $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
-    })
-    
-  },
-  // method to decrement counter and count unanswered if timer runs out
-  timerRunning : function(){
-    // if timer still has time left and there are still questions left to ask
-    if(game.timer > -1 && game.currentSet < Object.keys(game.questions).length){
-      $('#timer').text(game.timer);
-      trivia.timer--;
-        if(trivia.timer === 4){
-          $('#timer').addClass('last-seconds');
+      var correctCount = 0;
+      var wrongCount = 0;
+      var unanswerCount = 0;
+      var timer = 20;
+      var intervalId;
+      var userGuess ="";
+      var running = false;
+      var qCount = options.length;
+      var pick;
+      var index;
+      var newArray = [];
+      var holder = [];
+      
+      
+      
+      $("#reset").hide();
+      //click start button to start game
+      $("#start").on("click", function () {
+          $("#start").hide();
+          displayQuestion();
+          runTimer();
+          for(var i = 0; i < options.length; i++) {
+        holder.push(options[i]);
+      }
+        })
+      //timer start
+      function runTimer(){
+        if (!running) {
+        intervalId = setInterval(decrement, 1000); 
+        running = true;
         }
-    }
-    // the time has run out and increment unanswered, run result
-    else if(game.timer === -1){
-      game.unanswered++;
-      game.result = false;
-      clearInterval(game.timerId);
-      resultId = setTimeout(game.guessResult, 1000);
-      $('#results').html('<h3>Out of time! The answer was '+ Object.values(game.answers)[game.currentSet] +'</h3>');
-    }
-    // if all the questions have been shown end the game, show results
-    else if(game.currentSet === Object.keys(game.questions).length){
+      }
+      //timer countdown
+      function decrement() {
+        $("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
+        timer --;
       
-      // adds results of game (correct, incorrect, unanswered) to the page
-      $('#results')
-        .html('<h3>Thank you for playing!</h3>'+
-        '<p>Correct: '+ game.correct +'</p>'+
-        '<p>Incorrect: '+ game.incorrect +'</p>'+
-        '<p>Unaswered: '+ game.unanswered +'</p>'+
-        '<p>Please play again!</p>');
+        //stop timer if reach 0
+        if (timer === 0) {
+          unanswerCount++;
+          stop();
+          $("#answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+          hidepicture();
+        }	
+      }
       
-      // hide game sction
-      $('#game').hide();
+      //timer stop
+      function stop() {
+        running = false;
+        clearInterval(intervalId);
+      }
+      //randomly pick question in array if not already shown
+      //display question and loop though and display possible answers
+      function displayQuestion() {
+        //generate random index in array
+        index = Math.floor(Math.random()*options.length);
+        pick = options[index];
       
-      // show start button to begin a new game
-      $('#start').show();
-    }
-    
-  },
-  // method to evaluate the option clicked
-  guessChecker : function() {
-    
-    // timer ID for gameResult setTimeout
-    var resultId;
-    
-    // the answer to the current question being asked
-    var currentAnswer = Object.values(game.answers)[game.currentSet];
-    
-    // if the text of the option picked matches the answer of the current question, increment correct
-    if($(this).text() === currentAnswer){
-      // turn button green for correct
-      $(this).addClass('btn-success').removeClass('btn-info');
+      //	if (pick.shown) {
+      //		//recursive to continue to generate new index until one is chosen that has not shown in this game yet
+      //		displayQuestion();
+      //	} else {
+      //		console.log(pick.question);
+          //iterate through answer array and display
+          $("#questionblock").html("<h2>" + pick.question + "</h2>");
+          for(var i = 0; i < pick.choice.length; i++) {
+            var userChoice = $("<div>");
+            userChoice.addClass("answerchoice");
+            userChoice.html(pick.choice[i]);
+            //assign array position to it so can check answer
+            userChoice.attr("data-guessvalue", i);
+            $("#answerblock").append(userChoice);
+      //		}
+      }
       
-      game.correct++;
-      clearInterval(game.timerId);
-      resultId = setTimeout(game.guessResult, 1000);
-      $('#results').html('<h3>Correct Answer!</h3>');
-    }
-    // else the user picked the wrong option, increment incorrect
-    else{
-      // turn button clicked red for incorrect
-      $(this).addClass('btn-danger').removeClass('btn-info');
       
-      game.incorrect++;
-      clearInterval(game.timerId);
-      resultId = setTimeout(game.guessResult, 1000);
-      $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
-    }
-    
-  },
-  // method to remove previous question results and options
-  guessResult : function(){
-    
-    // increment to next question set
-    game.currentSet++;
-    
-    // remove the options and results
-    $('.option').remove();
-    $('#results h3').remove();
-    
-    // begin next question
-    game.nextQuestion();
-     
-  }
-
-}
-
-
+      
+      //click function to select answer and outcomes
+      $(".answerchoice").on("click", function () {
+        //grab array position from userGuess
+        userGuess = parseInt($(this).attr("data-guessvalue"));
+      
+        //correct guess or wrong guess outcomes
+        if (userGuess === pick.answer) {
+          stop();
+          correctCount++;
+          userGuess="";
+          $("#answerblock").html("<p>Correct!</p>");
+          hidepicture();
+      
+        } else {
+          stop();
+          wrongCount++;
+          userGuess="";
+          $("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+          hidepicture();
+        }
+      })
+      }
+      
+      
+      function hidepicture () {
+        $("#answerblock").append("<img src=" + pick.photo + ">");
+        newArray.push(pick);
+        options.splice(index,1);
+      
+        var hidpic = setTimeout(function() {
+          $("#answerblock").empty();
+          timer= 20;
+      
+        //run the score screen if all questions answered
+        if ((wrongCount + correctCount + unanswerCount) === qCount) {
+          $("#questionblock").empty();
+          $("#questionblock").html("<h3>Game Over!  Here's how you did: </h3>");
+          $("#answerblock").append("<h4> Correct: " + correctCount + "</h4>" );
+          $("#answerblock").append("<h4> Incorrect: " + wrongCount + "</h4>" );
+          $("#answerblock").append("<h4> Unanswered: " + unanswerCount + "</h4>" );
+          $("#reset").show();
+          correctCount = 0;
+          wrongCount = 0;
+          unanswerCount = 0;
+      
+        } else {
+          runTimer();
+          displayQuestion();
+      
+        }
+        }, 3000);
+      
+      
+      }
+      
+      $("#reset").on("click", function() {
+        $("#reset").hide();
+        $("#answerblock").empty();
+        $("#questionblock").empty();
+        for(var i = 0; i < holder.length; i++) {
+          options.push(holder[i]);
+        }
+        runTimer();
+        displayQuestion();
+      
+      })
+      
+      })
